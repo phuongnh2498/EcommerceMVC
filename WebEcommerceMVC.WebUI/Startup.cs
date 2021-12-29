8c6f3a3130f5764e77a8d30cfa6e377214dab111
phuongnh2498 <phuongnh2498@gmail.com>
2021-12-29 23:02:39 +0700

    Add project files.

---
+++ WebEcommerceMVC.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebEcommerceMVC.WebUI.Startup))]
+namespace WebEcommerceMVC.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
